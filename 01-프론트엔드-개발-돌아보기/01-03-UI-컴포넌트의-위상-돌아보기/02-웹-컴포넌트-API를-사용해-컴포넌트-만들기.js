e5f3f10f758bc6b9407fa32a490893ebd32cc4a5
Miles Kim <kyw05171@gmail.com>
2025-10-02 00:20:51 +0900

    study 02-웹-컴포넌트-API를-사용해-컴포넌트-만들기

---
+++ 01-프론트엔드-개발-돌아보기/01-03-UI-컴포넌트의-위상-돌아보기/02-웹-컴포넌트-API를-사용해-컴포넌트-만들기.js
@@ -0,0 +1,112 @@
+class LazyImageLoader extends HTMLElement {
+  constructor() {
+    super();
+
+    this._image = document.createElement('img');
+    this._image.style.opacity = '0';
+    this._image.style.transition = 'opacity 1s ease-in-out';
+
+    this._spinner = document.createElement('div');
+    this._spinner.className = 'loader';
+    this._spinner.style.display = 'block';
+
+    const indicatorColor = this.getAttribute('indicator-color');
+    if (indicatorColor) {
+      this._spinner.style.borderTopColor = indicatorColor;
+    }
+
+    this.applyBlur = this.hasAttribute('apply-blur');
+
+    this.observer = new IntersectionObserver(this.handleIntersection.bind(this), {
+      rootMargin: '0px 0px 200px 0px',
+      threshold: 0,
+    });
+  }
+
+  static get observerAttributes() {
+    return ['border-radius'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'border-radius') {
+      this._image.style.borderRadius = newValue;
+    }
+  }
+
+  getObserver() {
+    return this.observer;
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+    const style = document.createElement('style');
+    style.textContent = `
+      .loader {
+        width: 50px;
+        height: 50px;
+
+        border: 16px sold #f3f3f3;
+        border-top: 16px solid blue;
+        border-radius: 50%;
+
+        animation: spin 2s linear infinite;
+      }
+
+      @keyframes spin {
+        from { transform: rotate(0deg); }
+        to { transform: rotate(360deg); }
+      }
+    `;
+
+    shadowRoot.appendChild(style);
+    shadowRoot.appendChild(this._spinner);
+    shadowRoot.appendChild(this._image);
+
+    this.observer.observe(this);
+  }
+
+  disconnectedCallback() {
+    this.observer.unobserve(this);
+  }
+
+  handleIntersection(entries) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        this.loadImage();
+        this.observer.unobserve(this);
+      }
+    });
+  }
+
+  loadImage() {
+    const networkSpeed = this.dataset.networkSpeed;
+
+    let delay = 0;
+
+    if (networkSpeed === 'slow3g') {
+      delay = 2_000;
+    } else if (networkSpeed === '2g') {
+      delay = 5_000;
+    }
+
+    if (this.applyBlur) {
+      this.style.filter = 'blur(10px)';
+    }
+
+    setTimeout(() => {
+      this._image.src = this.dataset.src;
+      this._image.onload = () => {
+        this._image.style.opacity = '1';
+        this.style.filter = 'blur(0)';
+        this._spinner.style.display = 'none';
+      }
+    }, delay);
+  }
+
+  reset() {
+    this._image.src = '';
+    this._image.style.opacity = '0';
+    this._image.style.display = 'block';
+    this._spinner.style.display = 'block';
+  }
+}
